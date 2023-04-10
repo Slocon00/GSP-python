f7dabd858b74e31836661c019d7d9d2da19db65e
Sloconyan <79758160+Sloconyan@users.noreply.github.com>
2023-04-10 19:30:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GSP python
+A python implementation of Generalized Sequential Pattern (GSP) algorithm
+for sequential pattern mining.
---
+++ gsp.py
@@ -0,0 +1,13 @@
+class GSP:
+
+    def __init__(self):
+        pass
+
+    def run_gsp(self):
+        pass
+
+    def generate_candidates(self):
+        pass
+
+    def prune_candidates(self):
+        pass
