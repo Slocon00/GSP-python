f2e03e79c21182be6202f7eea7a1183166564794
Sloconyan <79758160+Sloconyan@users.noreply.github.com>
2023-04-10 20:01:06 +0200

    Add Sequence class, frequent 1-sequence generation

--- gsp.py
+++ gsp.py
@@ -5,11 +5,20 @@ class GSP:
 
     def __init__(self):
         self.db = []
+        self.frequent_sequences = {}
+        self.candidate_sequences = []
 
     def run_gsp(self, input_filename, output_filename, minsup):
         """Run GSP algorithm with the given minsup parameter"""
         if not self.load_db(input_filename):
             return
+
+        """Find all frequent 1-sequences"""
+        for item in self.frequent_sequences:
+            support_count = len(self.frequent_sequences[item][0].set_of_indexes)
+            support = support_count / len(self.db)
+            if support < minsup:
+                self.frequent_sequences[item].clear()
 
     def generate_candidates(self):
         pass
@@ -30,12 +39,14 @@ class GSP:
         sequence = []
         element = []
 
+        index = 0
         for char in chars:
             if char == "-2":
                 """Character marks end of sequence"""
                 self.db.append(sequence)
                 sequence = []
                 element = []
+                index += 1
             elif char == "-1":
                 """Character marks end of element"""
                 sequence.append(element)
@@ -45,4 +56,16 @@ class GSP:
                 item = int(char)
                 element.append(item)
 
+                if item not in self.frequent_sequences:
+                    self.frequent_sequences[item] = [Sequence()]
+                    self.frequent_sequences[item][0].itemsets.append([item])
+                self.frequent_sequences[item][0].set_of_indexes.add(index)
+
         return True
+
+
+class Sequence:
+
+    def __init__(self):
+        self.itemsets = []
+        self.set_of_indexes = set()
