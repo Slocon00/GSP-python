2cc068c6cec5c9b85cd9d04a4036dcd692c3c497
Sloconyan <79758160+Sloconyan@users.noreply.github.com>
2023-04-10 19:33:52 +0200

    Add input file parsing

--- gsp.py
+++ gsp.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
+
 class GSP:
 
     def __init__(self):
-        pass
+        self.db = []
 
     def run_gsp(self):
         pass
@@ -11,3 +14,33 @@ class GSP:
 
     def prune_candidates(self):
         pass
+
+    def load_db(self, input_filename):
+        """Store in db the sequence database contained in input_filename"""
+        path = Path(input_filename)
+        try:
+            content = path.read_text()
+        except FileNotFoundError:
+            print("File", input_filename, "not found.")
+            return False
+
+        chars = content.split()
+        sequence = []
+        element = []
+
+        for char in chars:
+            if char == "-2":
+                """Character marks end of sequence"""
+                self.db.append(sequence)
+                sequence = []
+                element = []
+            elif char == "-1":
+                """Character marks end of element"""
+                sequence.append(element)
+                element = []
+            else:
+                """Character is an item"""
+                item = int(char)
+                element.append(item)
+
+        return True
