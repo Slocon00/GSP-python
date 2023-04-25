7350dc95c1068332ba32b9cd198ee5adc0aac6f7
Sloconyan <79758160+Sloconyan@users.noreply.github.com>
2023-04-25 12:12:08 +0200

    Add a simple random sequence database generation

---
+++ database_gen.py
@@ -0,0 +1,58 @@
+import random
+import sys
+import logging
+logging.basicConfig(level=logging.INFO, stream=sys.stdout,
+                    format="%(levelname)s:%(message)s")
+
+
+class DatabaseGenerator:
+    """A class that represents a sequence database generator.
+
+    An instance of the DatabaseGenerator class is initialized with the
+    requirements the desired sequence database must meet, and the
+    generate_sequence_database method randomly generates and returns a
+    sequence database as a list[list[list[int]]].
+    """
+
+    def __init__(self, size, nevents, maxevents, maxelems, seed=None, verbose=False):
+        """Initialize the instance with the given requirements:
+
+            size: overall number of sequences in the database
+            nevents: number of unique events in the database
+            maxevents: max number of events contained in a single element
+            maxelems: max number of elements contained in a single sequence
+            seed: the seed to be used for random number generation (if not
+            provided, random's default choice of seed is used)
+        """
+        self.output = []
+
+        self.size = size
+        self.nevents = nevents
+        self.maxevents = maxevents
+        self.maxelems = maxelems
+
+        if seed is not None:
+            random.seed(seed)
+
+        if not verbose:
+            logging.disable()
+
+    def generate_sequence_database(self):
+        """Generate a simple sequence database"""
+        for i in range(self.size):
+            sequence = []
+            for j in range(1, random.randint(1, self.maxelems) + 1):
+                """Element must not contain duplicate events, events are sorted
+                in ascending order
+                """
+                element = set()
+                for k in range(1, random.randint(1, self.maxevents) + 1):
+                    element.add(random.randint(1, self.nevents))
+
+                logging.info(f"Element: {element}")
+                sequence.append(list(element))
+
+            logging.info(f"Sequence: {sequence}")
+            self.output.append(sequence)
+
+        return self.output
