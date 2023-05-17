0edafe5a8a58e72398de0b203fa00ac0299e22bc
Sloconyan <79758160+Sloconyan@users.noreply.github.com>
2023-05-17 11:08:35 +0200

    Changed maxelems parameter to avgelems
    Parameter now lets the user specify the average length of a single
    sequence in the generated database.

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def setup_subparsers(parser):
     parser_dbgen.add_argument('size', type=int, help='# of sequences')
     parser_dbgen.add_argument('nevents', type=int, help='# of unique events')
     parser_dbgen.add_argument('maxevents', type=int, help='max # of events in an element')
-    parser_dbgen.add_argument('maxelems', type=int, help='max # of elements in a sequence')
+    parser_dbgen.add_argument('avgelems', type=int, help='average # of elements in a sequence')
     parser_dbgen.add_argument('-s', '--seed', default=None, help='seed for random event generation')
     parser_dbgen.add_argument('-v', '--verbose', action='store_true',
                               help='enable printing of debug messages')
@@ -94,7 +94,7 @@ def main(argv):
 
         """Generating database"""
         algo_obj = DatabaseGenerator(parsed_argv.size, parsed_argv.nevents,
-                                     parsed_argv.maxevents, parsed_argv.maxelems,
+                                     parsed_argv.maxevents, parsed_argv.avgelems,
                                      parsed_argv.seed, parsed_argv.verbose)
         result = algo_obj.generate_sequence_database()
 
