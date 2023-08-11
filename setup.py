d02e69d9d76c1f3c989e81026838684d50b7219b
Slocon00 <79758160+Slocon00@users.noreply.github.com>
2023-08-11 17:49:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.9'
+VERSION = '0.0.10'
 DESCRIPTION = 'GSP Python implementation'
 LONG_DESCRIPTION = 'A Python implementation of Generalized Sequential Patterns (GSP) algorithm for sequential pattern mining'
 
